0430eaf2c72b64508a520c80b05e25b51b082cd8
Oleksandr_Zhynkin <godinsighted@gmail.com>
2024-07-21 18:07:22 +0300

    done

--- src/components/RestrictedRoute/RestrictedRoute.tsx
+++ src/components/RestrictedRoute/RestrictedRoute.tsx
@@ -2,8 +2,9 @@ import { useSelector } from "react-redux";
 import { Navigate, useLocation } from "react-router-dom";
 
 import { selectIsLoggedIn } from "../../redux/auth/selectors";
+import { PropChildren } from "../../types/types";
 
-const RestrictedRoute = ({ children }) => {
+const RestrictedRoute = ({ children }: PropChildren) => {
   const location = useLocation();
   const isLoggedIn = useSelector(selectIsLoggedIn);
   return isLoggedIn ? (
